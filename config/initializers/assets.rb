7c049a144fd4157055046c4d7dd9c0c6368de6d2
Asim Abdul <asim@viralheat.com>
2015-12-06 10:50:52 -0800

    compiling every controller js file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+
+Rails.application.config.assets.precompile += %w[events_controller.js scores_controller.js tasting_packages_controller.js]
+
 # Rails.application.config.assets.precompile += %w( search.js )
